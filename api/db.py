2a5c9d0c6b84592df223a563c88dcb21dadc0ffc
yitimo <admin@yitimo.com>
2018-02-26 16:43:51 +0800

    update db setting

--- api/db.py
+++ api/db.py
@@ -7,7 +7,7 @@ MONGOUSER = os.environ.get('MONGOUSER')
 MONGOPASS = os.environ.get('MONGOPASS')
 MONGOURL = os.environ.get('MONGOURL')
 MONGOAPP = os.environ.get('MONGOAPP')
-MONGOURL = "mongodb://{}:{}@{}/{}".format(MONGOUSER, MONGOPASS, MONGOURL, MONGOAPP)
+MONGOURL = "mongodb://127.0.0.1:27017"
 
 client = MongoClient(MONGOURL)
 db = client[MONGOAPP]
@@ -19,7 +19,7 @@ db = client[MONGOAPP]
 
 redis_url = os.environ.get('REDISTOGO_URL')
 
-redis_db = redis.from_url(redis_url)
+redis_db = redis.from_url("redis://127.0.0.1:6379")
 
 # redis_db.flushdb()
 
