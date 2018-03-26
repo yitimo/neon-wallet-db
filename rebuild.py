5d71fb6836316800df7063e7be922d1782bcf348
yitimo <admin@yitimo.com>
2018-03-26 11:50:46 +0800

    update height

--- rebuild.py
+++ rebuild.py
@@ -3,5 +3,5 @@ from rq import Queue
 from api import redis_db as conn
 from api.blockchain import storeLatestBlockInDB, getBlockCount, blockchain_db, storeBlockInDB, checkSeeds, get_highest_node
 
-for i in range(0,0) :
+for i in range(0,135) :
   storeBlockInDB(i)
